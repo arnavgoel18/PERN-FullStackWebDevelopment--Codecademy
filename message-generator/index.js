49d88801519dfc51cc9e56144cfcf889f2b71e02
arnavgoel18 <arnav18.goel@gmail.com>
2021-05-30 09:41:08 +0530

    project incomplete

---
+++ message-generator/index.js
@@ -0,0 +1,81 @@
+/* 
+    Message Generator
+    ------------------
+    Generate Messages for F1
+*/
+
+const formulaGrid = {
+    teams:{
+        mercedes:{
+            drivers: {
+                Lewis: "I would like some comptetion",
+                Bottas: "Everyone thinks i come second"
+            },
+            teamPrincipal: "I am Totto Wolff"
+        },
+        ferrari:{
+            drivers: {
+                Carlos: "I am Spanish. I love Lando",
+                Leclerc: "Monaco: I love you, I hate you. I hate that I love you"
+            },
+            teamPrincipal: "I am Mattia Binotto"
+        },
+        red_bull:{
+            drivers: {
+                Verstappen: "I'm stupid",
+                Checho: "I'm the king of tire management"
+            },
+            teamPrincipal: "I am Christian Horner"
+        },
+        alpha_tauri:{
+            drivers: {
+                Yuki: "Chota Packet Bada Dhamaka!",
+                Gasly: "I hate red bull"
+            },
+            teamPrincipal: "No one knows me"
+        },
+        mclaren:{
+            drivers: {
+                Lando: "It's Friday thennnn, Saturday Sunday Whatttt",
+                Riccardo: "I'm the funniest F1 Driver everrrrr"
+            },
+            teamPrincipal: "I am Zac Brown"
+        },
+        aston_martin:{
+            drivers: {
+                Vettel: "What happened to me :(",
+                Stroll: "Daddy's Cash"
+            },
+            teamPrincipal: "I dont like the regulations so i will cry"
+        },
+        apline:{
+            drivers: {
+                Ocon: "I want Mercedessss",
+                Alonso: "How's this for a comeback?"
+            },
+            teamPrincipal: "I'm the new guy! I'm fckn serious"
+        },
+        alpha_romero:{
+            drivers: {
+                Kimi: "I'm a robot",
+                Antonio: "Wait and watch what i'm gonna do"
+            },
+            teamPrincipal: "I'm a fat fuck"
+        },
+        haas:{
+            drivers: {
+                Mick: "I have a therapist as my Race Engineer",
+                Nikita: "I am MazaSpin cause i like to Spin. Thankfully in Monoco, I didnt go for a Swim"
+            },
+            teamPrincipal: "I'm a big idiot! I dont know why I am doing this. Smone help me"
+        },
+        williams:{
+            drivers: {
+                Russel: "I will fuck up Verstappen",
+                Latifi: "I love beer and football"
+            },
+            teamPrincipal: "No one ever asks me who i am"
+        }
+    }
+}
+
